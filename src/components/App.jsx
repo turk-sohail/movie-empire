b90a7b01ca710b336745ce223058ccf9675ca341
turk-sohail <ksohail470@gmail.com>
2023-06-02 10:57:05 +0500

    added app structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,38 @@
+/* eslint-disable no-console */
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+import { Routes, Route, Link } from 'react-router-dom';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import useStyles from './styles';
+import {
+  Actors,
+  Movies,
+  MovieInformation,
+  Profile,
+  NavBar,
+  NotFound,
+} from './index';
 
 function App() {
-  return <h2>hello worl</h2>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar></NavBar>
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path='/' element={<Movies />}></Route>
+          <Route path='/actors' element={<Actors />}></Route>
+
+          <Route path='/profile/:id' element={<Profile />}></Route>
+
+          <Route path='/movie/:id' element={<MovieInformation />}></Route>
+          <Route path='*' element={<NotFound />}></Route>
+        </Routes>
+      </main>
+    </div>
+  );
 }
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+
+import React from 'react';
+const Home = () => {
+  return <h2>home page</h2>;
+};
+
+export default Home;
